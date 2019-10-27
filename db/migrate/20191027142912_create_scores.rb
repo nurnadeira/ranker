809639d081c9ba8a1ed97bc5888f3bc970913322
mfrashad <m.fathyrashad@gmail.com>
2019-10-27 22:30:52 +0800

    Mdodify evaluator foreign key

--- db/migrate/20191027142912_create_scores.rb
+++ db/migrate/20191027142912_create_scores.rb
@@ -3,7 +3,7 @@ class CreateScores < ActiveRecord::Migration[5.2]
     create_table :scores do |t|
       t.string :name
       t.integer :value
-      t.references :evaluator, foreign_key: true
+      t.references :evaluator, foreign_key: { to_table: 'users' }
       t.references :user, foreign_key: true
 
       t.timestamps
