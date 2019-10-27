74ed71ea14111da4a73b9b5db5c1326c0d4bf4a3
mfrashad <m.fathyrashad@gmail.com>
2019-10-27 16:31:22 +0800

    Migrate skill

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_27_020618) do
+ActiveRecord::Schema.define(version: 2019_10_27_082907) do
+
+  create_table "skills", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_skills_on_user_id"
+  end
 
   create_table "user_profiles", force: :cascade do |t|
     t.string "name"
