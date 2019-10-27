12efdb685aa7407ecf351fef9369847e8353273c
mfrashad <m.fathyrashad@gmail.com>
2019-10-27 10:06:31 +0800

    Scaffold user profile

---
+++ db/migrate/20191027020618_create_user_profiles.rb
@@ -0,0 +1,13 @@
+class CreateUserProfiles < ActiveRecord::Migration[5.2]
+  def change
+    create_table :user_profiles do |t|
+      t.string :name
+      t.text :bio
+      t.string :experience
+      t.string :phone
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
